954813a9fc53505c0e14830338fbaf7bf7b8fb31
SogogiBoB <jjh9240@naver.com>
2025-04-13 01:24:45 +0900

    just test

--- js/weather.js
+++ js/weather.js
@@ -3,6 +3,7 @@ function onGeoOk(position) {
     const lng = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
+        console.log(data);
         const weather = document.querySelector("#weather span:nth-child(2)");
         weather.innerText = `${data.weather[0].main} / ${Math.floor(data.main.temp)}°C`;
         const city = document.querySelector("#weather span:last-child");
